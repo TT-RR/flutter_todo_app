5e27966f03c363c0b43e78fe295af892a1d3cdc3
TT-RR <kaiza1mage@gmail.com>
2023-01-18 16:31:40 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,36 @@ class ToDoListPage extends StatelessWidget {
       body: Center(
         child: Text('リスト画面'),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          // "push"で新規画面に遷移
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              // 遷移先の画面としてリスト追加画面を指定
+              return TodoAddPage();
+            }),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// リスト追加画面用Widget
+class TodoAddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          // ボタンをクリックした時の処理
+          onPressed: () {
+            // "pop"で前の画面に戻る
+            Navigator.of(context).pop();
+          },
+          child: Text('リスト追加画面（クリックで戻る）'),
+        ),
+      ),
     );
   }
 }
